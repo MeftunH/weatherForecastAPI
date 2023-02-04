a9f8346b490225bfd14d2629d972520d352adf5b
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-04 23:42:12 +0300

    set validation package

---
+++ src/main/java/com/weatherforecastapi/entity/daily/DailyRoot.java
@@ -0,0 +1,28 @@
+package com.weatherforecastapi.entity.daily;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+
+@Getter
+@Setter
+public class DailyRoot {
+    private Coord coord;
+    private ArrayList<Weather> weather;
+    private String base;
+    private Main main;
+    private int visibility;
+    private Wind wind;
+    private Clouds clouds;
+    private int dt;
+    private Sys sys;
+    private int timezone;
+    private int id;
+
+    @NotNull("city name can not be null")
+    private String name;
+
+    private int cod;
+}
