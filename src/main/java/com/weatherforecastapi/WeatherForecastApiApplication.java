b95a4d4a3c39db13940825deae19206d9c1809b3
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-04 23:43:14 +0300

    change package

--- src/main/java/com/weatherforecastapi/WeatherForecastApiApplication.java
+++ src/main/java/com/weatherforecastapi/WeatherForecastApiApplication.java
@@ -1,4 +1,4 @@
-package com.example.weatherforecastapi;
+package com.weatherforecastapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
